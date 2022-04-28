c21c0ac4ec72a634bb7794fdc8a31a09f3a286bb
HOLGERILLAPA <holgerpatricio@gmail.com>
2022-04-27 23:57:31 -0500

    Agregar archivos de proyecto.

---
+++ HolgerIllapa/Login.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HolgerIllapa
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "holger2022" && txtpass.Text == "uisrael2022")
+            {
+
+                DisplayAlert("BIENVENIDOS","Registro de Estudiante", "OK");
+                Navigation.PushAsync(new Registro());
+
+
+            }
+            else
+            {
+                DisplayAlert("Error", "Usuario o Clave Incorrecta", "ok");
+            }
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ HolgerIllapa/Registro.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HolgerIllapa
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+
+    {
+        Double porcentaje;
+        double Pagomensual;
+        public Registro()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+
+        {
+            if (int.Parse(TxtMontoin.Text) >= 3000)
+             {
+                DisplayAlert("Error", "La cuota inicial debe ser menor que 3000", "Cerrar");
+                return;
+
+            }
+            porcentaje = 3000 * 0.05;
+            Pagomensual = ((3000 - double.Parse(TxtMontoin.Text)))/ 5 + porcentaje;
+            TxtMontoin.Text = TxtPagoMensual.ToString();
+
+        }
+
+        private async void btnGuardar_Clicked(object sender, EventArgs e)
+
+        {
+            if (TexNombre.Text == null)
+            {
+                await DisplayAlert("Error", "Debe ingresar un nombre", "Cerrar");
+                return;
+            }
+            await DisplayAlert("Guardado", "Elemento gardado con éxito", "Ok");
+            double total = Convert.ToDouble(TxtMontoin.Text) + Pagomensual * 5;
+            //await Navigation.PushAsync(new Resumen(TexNombre.Text, total));
+        }
+    }
+}
